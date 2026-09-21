085c0e4fc29c30a09145cbe5ba77bb5ca0bdb990
DannSrs <danniels2801@gmail.com>
2026-09-21 17:22:04 -0300

    feat: adiciona primeiro observador na Screen para imprimir digito recente

--- telephone/Screen.java
+++ telephone/Screen.java
@@ -8,5 +8,16 @@ public class Screen {
 
     public Screen(PhoneModel model) {
         this.model = model;
+        
+        // Cria o primeiro observador: imprime apenas o dígito mais recente
+        Observer observadorDigito = new Observer() {
+            @Override
+            public void update(int newDigit) {
+                System.out.println(newDigit);
+            }
+        };
+        
+        // Cadastra o observador no modelo
+        this.model.addObserver(observadorDigito);
     }
 }
