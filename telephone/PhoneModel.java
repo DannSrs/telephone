c287a78092501497ff69bdc97186ef135269423e
DannSrs <danniels2801@gmail.com>
2026-09-21 17:20:50 -0300

    feat: adiciona lista e notificacao de observadores no PhoneModel

--- telephone/PhoneModel.java
+++ telephone/PhoneModel.java
@@ -6,9 +6,22 @@ import java.util.List;
  */
 public class PhoneModel {
     private List<Integer> digits = new ArrayList<>();
+    
+    // Lista para guardar os observadores
+    private List<Observer> observers = new ArrayList<>();
+
+    // Método para permitir que a Screen adicione observadores
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
 
     public void addDigit(int newDigit) {
         digits.add(newDigit);
+        
+        // Avisar a todos os observadores que um novo dígito chegou!
+        for (Observer observer : observers) {
+            observer.update(newDigit);
+        }
     }
 
     public List<Integer> getDigits() {
